3969e4c4d10b1ec1f2f9598fd1357a86ea713e04
Revenko Vladislav <revad8@gmail.com>
2023-11-06 12:31:04 +0300

    do half task from module 30

--- LiveSpot/Static/JS/about.js
+++ LiveSpot/Static/JS/about.js
@@ -3,10 +3,26 @@ function getDate(){
     return new Date().toLocaleString();
 }
 function makeComment(){
-    const name = prompt("Введите ваше имя: ");
-    let comment = prompt("Ваш отзыв: ")
-    let element = document.getElementsByClassName("comments")[0];
-    
-    element.innerHTML += '<p>'+'<b>'+name+'</b> '+'<i>'+getDate()+'</i>'+'<br>'+comment+'</p>'
+    let comment = new Object();
+    comment.name = prompt("Введите ваше имя: ");
+    comment.content = prompt("Ваш отзыв: ");
+    comment.date = getDate();
+    if(confirm("Хотите ли, что ваш коменнтарий могли оценить?")){
+        let likedComment = Object.create(comment)
+        likedComment.rate = 0;
+        writeComment(likedComment);
+    }
+    else {
+        writeComment(comment);
+    }
 }
 
+function writeComment(comment){
+    let element = document.getElementsByClassName("comments")[0];
+    if(comment.hasOwnProperty('rate')){
+        element.innerHTML += '<p>'+'<b>'+comment.name+'</b> '+'<i>'+comment.date+'</i> '+'<b style="color: crimson">Рейтинг комметария: '+comment.rate+'</b><br>'+comment.content+'</p>';
+    }
+    else{
+        element.innerHTML += '<p>'+'<b>'+comment.name+'</b> '+'<i>'+comment.date+'</i>'+'<br>'+comment.content+'</p>';
+    }
+}
--- LiveSpot/Static/JS/testing.js
+++ LiveSpot/Static/JS/testing.js
@@ -1,3 +1,8 @@
-﻿let elements = document.getElementsByTagName('*');
-
-alert(`Elements on page: ${elements.length}`)
+﻿
+function saveInput(){
+    let input = document.getElementsByTagName("input")[0].value;
+    this.lastInput;
+    alert(`Последний ввод был: ${this.lastInput} \nтекущий ввод: ${input}`);
+    this.lastInput = input;
+    
+}
