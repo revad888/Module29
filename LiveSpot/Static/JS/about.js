fe7999fda241323d730e27bedc6487e87f88947c
revad888 <revad8@gmail.com>
2023-11-06 14:44:23 +0300

    do task from 30.3

--- LiveSpot/Static/JS/about.js
+++ LiveSpot/Static/JS/about.js
@@ -1,12 +1,17 @@
-﻿
+﻿function Comment(author, text){
+    this.Id = Math.random();
+    this.author = author;
+    this.text = text;
+    this.date = new Date().toLocaleString();
+}
 function getDate(){
     return new Date().toLocaleString();
 }
 function makeComment(){
-    let comment = new Object();
-    comment.name = prompt("Введите ваше имя: ");
-    comment.content = prompt("Ваш отзыв: ");
-    comment.date = getDate();
+    
+    const name = prompt("Введите ваше имя: ");
+    const content = prompt("Ваш отзыв: ");
+    const comment = new Comment(name, content);
     if(confirm("Хотите ли, что ваш коменнтарий могли оценить?")){
         let likedComment = Object.create(comment)
         likedComment.rate = 0;
@@ -16,13 +21,26 @@ function makeComment(){
         writeComment(comment);
     }
 }
+function addLike(id){
+    const comment = document.getElementById(id);
+    let innerText = comment.innerText.split(" ");
+    let newCounter = parseInt(innerText[innerText.length-1], 10)+1;
+    innerText[innerText.length-1] = `${newCounter}`;
+    comment.innerText = innerText.join(" ");
+    
+    
+}
 
 function writeComment(comment){
     let element = document.getElementsByClassName("comments")[0];
     if(comment.hasOwnProperty('rate')){
-        element.innerHTML += '<p>'+'<b>'+comment.name+'</b> '+'<i>'+comment.date+'</i> '+'<b style="color: crimson">Рейтинг комметария: '+comment.rate+'</b><br>'+comment.content+'</p>';
+        element.innerHTML += '<p>'+'<b>'+comment.author+'</b> '+
+            '<i>'+comment.date+'</i> '+
+            `<button id="${Math.random()}" onclick="addLike(this.id)">❤️ 0</button>`+
+            '</b><br>'+comment.text+'</p>';
     }
     else{
-        element.innerHTML += '<p>'+'<b>'+comment.name+'</b> '+'<i>'+comment.date+'</i>'+'<br>'+comment.content+'</p>';
+        element.innerHTML += '<p>'+'<b>'+comment.author+'</b> '+'<i>'+
+            comment.date+'</i>'+'<br>'+comment.text+'</p>';
     }
 }
